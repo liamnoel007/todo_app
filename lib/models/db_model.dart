0dea9716ab5f79093af3602e5a59f74641a44811
liamnoel007 <oasisamb@gmail.com>
2022-09-07 11:56:22 +0300

    1

--- lib/models/db_model.dart
+++ lib/models/db_model.dart
@@ -1,2 +1,11 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import './todo_model.dart';
+
+class DatabaseConnect {
+  Database? _database;
+
+  Future<Database> get database async {
+    final dbpath = await getDatabasesPath();
+  }
+}
