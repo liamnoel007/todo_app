4e74252c8676215e3e547e9e2c29118b41b9ee08
liamnoel007 <oasisamb@gmail.com>
2022-09-08 20:21:36 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_app/screens/home.dart';
 
 void main() {
@@ -10,7 +13,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       home: Home(),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,11 +9,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ToDo App'),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
         child: const Text('This is home screen'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpeg'),
+            ),
+          ),
+        ],
       ),
     );
   }
