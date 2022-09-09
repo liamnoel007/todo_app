df2b5dc2e68479f90393044224df534607f686b4
liamnoel007 <oasisamb@gmail.com>
2022-09-09 20:38:36 +0300

    1

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+      ),
+    );
+  }
+}
