aedae4c0bf8652a5248c409275ffbec023d4df89
liamnoel007 <oasisamb@gmail.com>
2022-09-11 00:58:13 +0300

    1

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -11,6 +11,7 @@ class ToDoItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
+        contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
         onTap: () {},
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
@@ -25,7 +26,23 @@ class ToDoItem extends StatelessWidget {
           style: TextStyle(
               color: tdBlack,
               fontSize: 18,
-              decoration: TextDecoration.underline),
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          width: 35,
+          height: 35,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5), color: tdRed),
+          child: IconButton(
+            icon: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            iconSize: 18,
+            onPressed: () {},
+          ),
         ),
       ),
     );
